f7b1a88aad65fc3e42605e5bd25e73f3332a277c
BroddyDumpcode <palpalehot69@gmail.com>
2026-09-16 00:16:19 +0700

    refactor: modularize extensions map into etc/ext.py and update fileForge.py

---
+++ etc/ext.py
@@ -0,0 +1,9 @@
+# etc/ext.py
+EXTENSIONS_MAP = {
+    "Images": [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".svg", ".webp", ".ico"],
+    "Documents": [".pdf", ".docx", ".doc", ".txt", ".xlsx", ".xls", ".pptx", ".csv", ".odt"],
+    "Archives": [".zip", ".rar", ".7z", ".tar", ".gz", ".bz2"],
+    "Code & Scripts": [".py", ".js", ".html", ".css", ".cpp", ".c", ".java", ".json", ".xml", ".sh"],
+    "Audio & Video": [".mp3", ".wav", ".mp4", ".mkv", ".avi", ".mov", ".flac"],
+    "Executables": [".exe", ".msi", ".deb", ".AppImage", ".apk", ".bat"]
+    }
